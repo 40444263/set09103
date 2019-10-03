d153540144b11de67c68fa93fa865259af31905b
Gregoire ARNOULT <40444263@napier.ac.uk>
2019-10-03 10:09:45 +0100

    test

--- filepy/test_git.py
+++ filepy/test_git.py
@@ -3,12 +3,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello Napier"
-@app.route(’/force404’)
+@app.route('/force404')
 def force404():
     abort(404)
 @app.errorhandler(404)
 def page_not_found(error):
-    return "Couldn’t find the page you requested.", 404
+    return "Couldn't find the page you requested.", 404
 
 if __name__ == "__main__":
-    app.run(host=’0.0.0.0’, debug=True)
+    app.run(host='0.0.0.0', debug=True)
