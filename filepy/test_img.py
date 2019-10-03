e09390c114476abbd8df8c3604c05999f0e7a0e8
Gregoire ARNOULT <40444263@napier.ac.uk>
2019-10-03 10:57:46 +0100

    10:57 03/10/2019 img fonctionne pas

--- filepy/test_img.py
+++ filepy/test_img.py
@@ -7,10 +7,10 @@ def hello():
 
 @app.route('/static-example/img')
 def static_example_img():
-    start = '<img src="../img'
+    start = '<img src="../set09103/img'
     url = url_for('static',filename='vmask.jpg')
     end = '">'
-    return start + url + end
+    return start + url + end,200 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
