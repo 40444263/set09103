32fcc8e0b348abb65b979ca905cf17a95b2fcc7b
Arnoult <40444263@live.napier.ac.uk>
2019-10-10 10:39:02 +0100

    10h38 2

--- filepy/url.py
+++ filepy/url.py
@@ -8,5 +8,6 @@ def hello(name):
 @app.route("/add/<int:first>/<int:second>")
 def add(first, second):
     return str(first + second)
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)g
+    app.run(host='0.0.0.0', debug=True)
