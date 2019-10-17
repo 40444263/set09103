9175ef10b83cd1a907c740e6507c3dfa9a0827e2
Arnoult <40444263@live.napier.ac.uk>
2019-10-17 09:19:28 +0100

    9h19

--- filepy/second_page.py
+++ filepy/second_page.py
@@ -4,7 +4,7 @@ app = Flask ( __name__ )
 @app . route ("/ display /")
 def display () :
     return '<img src =" '+ url_for ('static ', filename ='uploads / file .png ') +'"/ > '
-    
+
 @app . route ("/ upload /", methods =[ 'POST ','GET '])
 def account () :
     if request . method == 'POST ':
@@ -12,18 +12,18 @@ def account () :
         f . save ('static / uploads / file .png ')
         return " File Uploaded "
     else :
-    page = '''
-    <html >
-    <body >
-    <form action ="" method =" post " name =" form " enctype =" multipart /
-        form-data " >
-        <input type =" file " name =" datafile " / >
-        <input type =" submit " name =" submit " id =" submit "/ >
-    </form >
-    </body >
-    </html >
-    '''
-    return page , 200
+        page = '''
+        <html >
+        <body >
+        <form action ="" method =" post " name =" form " enctype =" multipart /
+            form-data " >
+            <input type =" file " name =" datafile " / >
+            <input type =" submit " name =" submit " id =" submit "/ >
+        </form >
+        </body >
+        </html >
+        '''
+        return page , 200
 
 if __name__ == " __main__ ":
 app.run ( host ='0.0.0.0 ', debug = True )
