12ec3fab89199ed28a86429f83836860b9b138cf
arnoultg <gregoire.arnoult@free.fr>
2019-11-04 14:43:10 +0000

    14h43

--- project/home.py
+++ project/home.py
@@ -8,6 +8,10 @@ def root():
 @app.route("/home")
 def home():
     return render_template("home.html")
-    
+
+@app.route("/map")
+def map():
+    return "ok"
+
 if __name__ == " __main__ ":
     app.run ( host ='0.0.0.0 ', debug = True )
