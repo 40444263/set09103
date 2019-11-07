2ab43abe699c0b3ae36fbe3c1144db7afa6501f4
Arnoult <40444263@live.napier.ac.uk>
2019-11-07 10:45:23 +0000

    putqin

--- project/game.py
+++ project/game.py
@@ -1,10 +1,9 @@
 import os
 import pygame
-os.environ['SDL_VIDEODRIVER'] = 'dummy'
 
 def main():
     pygame.init()
-    fenetre = pygame.display.set_mode((1,1))
+    fenetre = pygame.display.set_mode()
     perso = pygame.image.load("perso.png").convert()
     fenetre.blit(perso, (200,300))
 
