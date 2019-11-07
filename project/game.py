8f304f7451c67eeafc577d191410b2c3f4452f1c
Arnoult <40444263@live.napier.ac.uk>
2019-11-07 10:47:33 +0000

    on fqit des test

--- project/game.py
+++ project/game.py
@@ -3,7 +3,7 @@ import pygame
 
 def main():
     pygame.init()
-    fenetre = pygame.display.set_mode()
+    fenetre = pygame.display.set_mode((800,800))
     perso = pygame.image.load("perso.png").convert()
     fenetre.blit(perso, (200,300))
 
