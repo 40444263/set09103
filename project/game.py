9f740d73297c15aa9b26c05deebab4992b95cac1
Arnoult <40444263@live.napier.ac.uk>
2019-11-07 10:48:30 +0000

    vfa

--- project/game.py
+++ project/game.py
@@ -7,10 +7,5 @@ def main():
     perso = pygame.image.load("perso.png").convert()
     fenetre.blit(perso, (200,300))
 
-    while 1:
-        events = pygame.event.get()
-        for e in events:
-            pass
-
 if __name__ == '__main__':
     main()
