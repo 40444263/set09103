d181fac28ae2516c2361c27a2e574b6617340046
Arnoult <40444263@live.napier.ac.uk>
2019-11-08 15:44:55 +0000

    15h44 jump a faire, image a mettre en fond et gerer la taille de l'ecran

--- project/static/game.js
+++ project/static/game.js
@@ -1,15 +1,18 @@
-var canvas = document.getElementById("myCanvas");
+var img = new Image();
+img.src = 'perso.png';
+
+var canvas = document.createElement('canvas');
+canvas.width = screen.width  //a changer
+canvas.height = screen.height//a changer
+
+document.body.appendChild(canvas);
 var ctx = canvas.getContext("2d");
-var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth)/2;
+var persoX = (canvas.width-img.width)/2;
+
 var rightPressed = false;
 var leftPressed = false;
+var upPressed = false;
+
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -21,6 +24,10 @@ function keyDownHandler(e) {
     else if(e.keyCode == 37) {
         leftPressed = true;
     }
+    else if(e.keyCode == 38) {
+
+        upPressed = true
+    }
 }
 function keyUpHandler(e) {
     if(e.keyCode == 39) {
@@ -29,38 +36,34 @@ function keyUpHandler(e) {
     else if(e.keyCode == 37) {
         leftPressed = false;
     }
+    else if(e.keyCode == 38) {
+        upPressed = false
+  }
 }
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+
+
+
+function draw_image(){
+  ctx.clearRect(persoX,canvas.height-img.height,img.width,img.height)
+  ctx.drawImage(img,persoX,canvas.height-img.height)
 }
-function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+
+function jump(){
+  
 }
+
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawPaddle();
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-        dx = -dx;
-    }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-        dy = -dy;
-    }
-    if(rightPressed && paddleX < canvas.width-paddleWidth) {
-        paddleX += 7;
-    }
-    else if(leftPressed && paddleX > 0) {
-        paddleX -= 7;
-    }
-    x += dx;
-    y += dy;
+  draw_image()
+  if(rightPressed && persoX < canvas.width-img.width) {
+      persoX += 7;
+  }
+  else if(leftPressed && persoX > 0) {
+      persoX -= 7;
+  }
+  else if(upPressed){
+    jump() //a faire
+  }
+  console.log(persoX)
 }
+
 setInterval(draw, 10);
