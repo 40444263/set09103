eec51678c3472e8b0e6f7c0b09a335cc845ecf93
Arnoult <40444263@live.napier.ac.uk>
2019-11-08 13:43:49 +0000

    gfea

--- project/static/game.js
+++ project/static/game.js
@@ -1,3 +1,10 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
@@ -23,7 +30,6 @@ function keyUpHandler(e) {
         leftPressed = false;
     }
 }
-
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -38,28 +44,23 @@ function drawPaddle() {
     ctx.fill();
     ctx.closePath();
 }
-
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     drawPaddle();
-
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
-
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
         paddleX += 7;
     }
     else if(leftPressed && paddleX > 0) {
         paddleX -= 7;
     }
-
     x += dx;
     y += dy;
 }
-
 setInterval(draw, 10);
