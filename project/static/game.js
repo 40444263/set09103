f58096ff1a310f616f09824186eb673a69f8e1ab
Arnoult <40444263@live.napier.ac.uk>
2019-11-14 12:26:55 +0000

    des bugs

--- project/static/game.js
+++ project/static/game.js
@@ -10,13 +10,17 @@ canvas.height = document.body.clientHeight
 document.body.appendChild(canvas);
 var ctx = canvas.getContext("2d");
 var persoX = (canvas.width-img.width)/2;
-var persoY = canvas.height-img.height;
+var persoY = canvas.height-(canvas.height*0.3)-img.height;
+var x_plat = canvas.width-(canvas.width*0.8)
+var y_plat = canvas.height-(canvas.height*0.3)
+
 
 var rightPressed = false;
 var leftPressed = false;
 var upPressed = false;
 
 var saut = false;
+var is_fall = false;
 var gravite = 0;
 
 
@@ -50,9 +54,16 @@ function keyUpHandler(e) {
 
 
 function draw_image(){
-
   ctx.clearRect(persoX,persoY,img.width,img.height);
   ctx.drawImage(img,persoX,persoY);
+}
+
+function draw_platform(){
+
+  ctx.beginPath();
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(x_plat, y_plat,canvas.width*0.6, 100);
+  ctx.stroke();
 }
 
 function jump(){
@@ -61,27 +72,61 @@ function jump(){
     persoY = persoY-(20-gravite)
     ctx.drawImage(img,persoX,persoY)
     gravite+=0.65
-    if (persoY > canvas.height-img.height){
-        persoY = canvas.height-img.height;
+    if ((persoY > canvas.height-(canvas.height*0.3)-img.height) && (persoX<canvas.width*0.8) && (persoX>(canvas.width*0.2-(img.width-5)))){
+        persoY = canvas.height-(canvas.height*0.3)-img.height;
         gravite=0
         saut = false
       }
-      setTimeout(jump,5)
+    else {
+      setTimeout(jump,5);
+      }
     }
+}
+
+function fall(){
+  if (is_fall && !saut){
+    ctx.clearRect(persoX,persoY,img.width,img.height)
+    persoY = persoY+gravite
+    ctx.drawImage(img,persoX,persoY)
+    if ((persoX<canvas.width*0.8) && (persoX>(canvas.width*0.2-(img.width-5))) && persoY <canvas.height*0.7) {
+     gravite=0
+     is_fall = false
+     console.log("fdoe")
+   }
+    else if (persoY > canvas.height){
+      gravite=0
+      is_fall = false
+    }
+    else{
+      gravite+=0.3
+      setTimeout(fall,10);
+    }
+  }
 }
 
 function draw() {
   draw_image()
+  draw_platform()
   if(rightPressed && persoX < canvas.width-img.width) {
       persoX += 7;
+      if (persoX>canvas.width*0.8 && !is_fall){
+        is_fall = true
+        fall()
+      }
   }
   else if(leftPressed && persoX > 0) {
       persoX -= 7;
+      if (persoX<(canvas.width*0.2-(img.width-5))&& !is_fall){
+        is_fall = true
+        fall()
+      }
   }
   if(upPressed){
     if (saut == false){
       saut = true;
-      jump() //a faire
+      gravite=0;
+      is_fall = false;
+      jump()
       console.log(" DRAW Y",persoY)
     }
   }
