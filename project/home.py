cb773e860457518c31793281aa8158c7fcebab84
40444263 <40444263@live.napier.ac.uk>
2019-12-02 14:04:32 +0000

    ds

--- project/home.py
+++ project/home.py
@@ -17,7 +17,7 @@ def home():
         curseur = connexion.cursor()
         data = curseur.execute("SELECT \"Path picture\" from Map where id =" + id +";")
         for row in data:
-            print(row)
+            print(row[0])
         # shutil.copy(data[0],"static/background.jpg")
         connexion.close()
     return render_template("home.html")
