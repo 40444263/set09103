47929c6a7f4fb779f6f922fbab6b70766d68129c
40444263 <40444263@live.napier.ac.uk>
2019-12-04 02:22:25 +0000

    fdsq

--- project/home.py
+++ project/home.py
@@ -58,7 +58,7 @@ def endgame():
     if (request.method == "POST"):
         connexion = sqlite3.connect("static/game.db")
         curseur = connexion.cursor()
-        data = curseur.execute("Insert into Match values("+request.form["Player1Win"]+","+request.form["Player2Win"]+","+request.form["NbHitPlayer1"]+","+request.form["NbHitPlayer2"]+","+request.form["Time"] + ");")
+        data = curseur.execute("Insert into Match (Player1Win,Player2Win,NbHitPlayer1,NbHitPlayer2,Time) values("+request.form["Player1Win"]+","+request.form["Player2Win"]+","+request.form["NbHitPlayer1"]+","+request.form["NbHitPlayer2"]+","+request.form["Time"] + ");")
         connexion.close()
     else:
         connexion = sqlite3.connect("static/game.db")
